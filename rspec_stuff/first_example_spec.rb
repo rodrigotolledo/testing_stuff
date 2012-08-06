c6e8d91c0d7fc532ac7ce52c6771fe800584407f
Rodrigo Tolledo <rodrigotolledo@gmail.com>
2012-08-05 21:28:15 -0300

    pending example (needs to implement MovieList)

--- rspec_stuff/first_example_spec.rb
+++ rspec_stuff/first_example_spec.rb
@@ -1,6 +1,7 @@
 describe "MovieList" do
 	context "when first created" do
 		it "is empty" do
+			pending "needs to implement MovieList"
 			movie_list = MovieList.new
 			movie_list.should be_empty
 		end
